0ad38fa7642942e61056cdcd8e38f399d79273dd
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-04-01 11:30:36 +0330

    Create method to scrap the https://shahroodut.ac.ir

--- src/main/java/com/example/studentserviceapplication/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/example/studentserviceapplication/service/impl/TeacherServiceImpl.java
@@ -1,8 +1,76 @@
 package com.example.studentserviceapplication.service.impl;
 
+import com.example.studentserviceapplication.domain.Faculty;
+import com.example.studentserviceapplication.domain.Teacher;
+import com.example.studentserviceapplication.domain.enumuration.Faculties;
+import com.example.studentserviceapplication.repository.TeacherRepository;
+import com.example.studentserviceapplication.service.FacultyService;
 import com.example.studentserviceapplication.service.TeacherService;
+import com.example.studentserviceapplication.service.dto.TeacherDTO;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
+
+    private final TeacherRepository teacherRepository;
+    private final FacultyService facultyService;
+
+    public TeacherServiceImpl(TeacherRepository teacherRepository, FacultyService facultyService) {
+        this.teacherRepository = teacherRepository;
+        this.facultyService = facultyService;
+    }
+
+    @Override
+    public TeacherDTO getTeacherById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<TeacherDTO> getAllTeachers() {
+        return null;
+    }
+
+    @Override
+    public List<Teacher> getAllTeachersFromWeb() {
+        for (int i = 0; i < Faculties.values().length; i++) {
+            Faculties faculty = Faculties.values()[i];
+            try {
+                String requiredFacultyUrl = "https://shahroodut.ac.ir/fa/schools/index.php?id=S" + faculty.getFacultyCode();/*faculties.get(1).attr("href") ;*/
+                Document requiredFacultyDoc = Jsoup.connect(requiredFacultyUrl).get();
+                Elements requiredTeachersPart = requiredFacultyDoc.select(".nav").select("ul").select("ul").select("li");
+                Set<Teacher> teacherSet = new HashSet<>();
+                for (int j = 0; j < 20; j++) {
+                    try {
+                        String requiredPartUrl = "https://shahroodut.ac.ir/fa/schools/index.php" + requiredTeachersPart.get(2).select("ul").select("li").get(j).select("a").attr("href");
+                        Document selectedPartUrl = Jsoup.connect(requiredPartUrl).get();
+                        Elements teachers = selectedPartUrl.select(".card");
+                        for (Element teacherItem : teachers) {
+                            String avatarUrl = teacherItem.select("img").attr("src").replace("..", "");
+                            String teacherFullName = teacherItem.select(".stafftitle").select("a").text();
+                            Faculty teacherFaculty =  facultyService.getFacultyByCode(faculty.getFacultyCode());
+                            Teacher teacher = new Teacher(teacherFullName, avatarUrl);
+                            teacher.setFaculty(teacherFaculty);
+                            teacherSet.add(teacher);
+                        }
+                    } catch (IndexOutOfBoundsException exception) {
+                        break;
+                    }
+                }
+                teacherRepository.saveAll(teacherSet);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  teacherRepository.findAll();
+    }
 }
