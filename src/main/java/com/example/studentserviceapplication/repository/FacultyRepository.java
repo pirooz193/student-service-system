0931cd8854fb98ec40a4ba977ba02688cc50ac5e
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-04-01 11:29:57 +0330

    Create methods to fetch required faculty from DB

--- src/main/java/com/example/studentserviceapplication/repository/FacultyRepository.java
+++ src/main/java/com/example/studentserviceapplication/repository/FacultyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FacultyRepository  extends JpaRepository<Faculty , Long> {
+    Faculty getFacultyByCode(String facultyCode) ;
 }
