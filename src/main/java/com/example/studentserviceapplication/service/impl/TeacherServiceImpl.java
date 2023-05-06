8b5a0025e3aa0ba9d36ac2a4b2c87520a85fcf31
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-06 17:42:58 +0330

    Refactor getAllTeachersFromWeb() method and separate responsibilities .

--- src/main/java/com/example/studentserviceapplication/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/example/studentserviceapplication/service/impl/TeacherServiceImpl.java
@@ -1,14 +1,23 @@
 package com.example.studentserviceapplication.service.impl;
 
+import com.example.studentserviceapplication.domain.Faculty;
 import com.example.studentserviceapplication.domain.Teacher;
+import com.example.studentserviceapplication.domain.enumuration.Faculties;
 import com.example.studentserviceapplication.repository.TeacherRepository;
+import com.example.studentserviceapplication.service.FacultyService;
 import com.example.studentserviceapplication.service.TeacherService;
 import com.example.studentserviceapplication.service.dto.TeacherDTO;
+import com.example.studentserviceapplication.service.mapper.FacultyMapper;
 import com.example.studentserviceapplication.service.mapper.TeacherMapper;
 import com.example.studentserviceapplication.web.error.NotFoundException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,10 +26,14 @@ import java.util.stream.Collectors;
 public class TeacherServiceImpl implements TeacherService {
 
     private final TeacherRepository teacherRepository;
+    private final FacultyService facultyService;
+    private final FacultyMapper facultyMapper;
     private final TeacherMapper teacherMapper;
 
-    public TeacherServiceImpl(TeacherRepository teacherRepository, TeacherMapper teacherMapper) {
+    public TeacherServiceImpl(TeacherRepository teacherRepository, FacultyService facultyService, FacultyMapper facultyMapper, TeacherMapper teacherMapper) {
         this.teacherRepository = teacherRepository;
+        this.facultyService = facultyService;
+        this.facultyMapper = facultyMapper;
         this.teacherMapper = teacherMapper;
     }
 
@@ -40,10 +53,11 @@ public class TeacherServiceImpl implements TeacherService {
 
     @Override
     public List<Teacher> getAllTeachersFromWeb() {
-        /*for (int i = 0; i < Faculties.values().length; i++) {
+        for (int i = 0; i < Faculties.values().length; i++) {
             Faculties faculty = Faculties.values()[i];
             try {
-                String requiredFacultyUrl = "https://shahroodut.ac.ir/fa/schools/index.php?id=S" + faculty.getFacultyCode();*//*faculties.get(1).attr("href") ;*//*
+                String requiredFacultyUrl = "https://shahroodut.ac.ir/fa/schools/index.php?id=S" + faculty.getFacultyCode();
+//                faculties.get(1).attr("href") ;
                 Document requiredFacultyDoc = Jsoup.connect(requiredFacultyUrl).get();
                 Elements requiredTeachersPart = requiredFacultyDoc.select(".nav").select("ul").select("ul").select("li");
                 for (int j = 0; j < 20; j++) {
@@ -56,13 +70,11 @@ public class TeacherServiceImpl implements TeacherService {
                             String teacherFullName = teacherItem.select(".stafftitle").select("a").text();
                             if (teacherRepository.findByFullName(teacherFullName).isEmpty()) {
                                 Teacher teacher = new Teacher(teacherFullName, avatarUrl);
-                                Faculty teacherFaculty = facultyService.getFacultyByCode(faculty.getFacultyCode());
+                                Faculty teacherFaculty = facultyMapper.toEntity(facultyService.getFacultyByCode(faculty.getFacultyCode()));
                                 teacherFaculty.getTeachers().add(teacher);
                                 teacherRepository.save(teacher);
-                                facultyRepository.save(teacherFaculty);
+                                facultyService.save(teacherFaculty);
                             }
-
-
                         }
                     } catch (IndexOutOfBoundsException exception) {
                         break;
@@ -72,8 +84,7 @@ public class TeacherServiceImpl implements TeacherService {
                 e.printStackTrace();
             }
         }
-        return  teacherRepository.findAll();*/
-        return null;
+        return teacherRepository.findAll();
     }
 
     @Override
