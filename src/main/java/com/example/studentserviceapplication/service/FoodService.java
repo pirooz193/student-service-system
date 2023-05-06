e1d295c580416061699f446404d6105b23cc3656
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-06 17:48:31 +0330

    Add Exception to method signature : MalformedURLException

--- src/main/java/com/example/studentserviceapplication/service/FoodService.java
+++ src/main/java/com/example/studentserviceapplication/service/FoodService.java
@@ -2,10 +2,11 @@ package com.example.studentserviceapplication.service;
 
 import com.example.studentserviceapplication.domain.Food;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 public interface FoodService {
     List<Food> getTodayFood();
 
-    void getAllFoodsFromWeb() throws InterruptedException;
+    void getAllFoodsFromWeb() throws InterruptedException, MalformedURLException;
 }
