676428302e2bb94d7de1894a6f0e1041bde84e86
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-07 10:35:03 +0330

    Update exception handling, separate methods to customize handles. - unexpected http errors will be handle in handleGeneralExceptions() method.

--- src/main/java/com/example/studentserviceapplication/web/error/ApplicationExceptionHandler.java
+++ src/main/java/com/example/studentserviceapplication/web/error/ApplicationExceptionHandler.java
@@ -1,34 +1,44 @@
 package com.example.studentserviceapplication.web.error;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
-    Logger logger = LogManager.getLogger(ApplicationExceptionHandler.class);
+    private static final Logger logger = LogManager.getLogger(ApplicationExceptionHandler.class);
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse> translateException(Exception exception) {
-        logger.info("Action failed caused by {}", exception.getCause());
-        if (exception instanceof HttpClientErrorException) {
-            return ResponseEntity.status(((HttpClientErrorException) exception).getStatusCode()).body(new ExceptionResponse(
-                    exception.getCause().getMessage(),
-                    ((HttpClientErrorException) exception).getStatusCode().value(),
-                    exception.getMessage()
+    @ExceptionHandler(value = {NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handleNotFoundException(NotFoundException ex, HttpServletRequest request) {
+        logger.error("Resource not found - URI: {}, Method: {}", request.getRequestURI(), request.getMethod(), ex);
+        return new ExceptionResponse(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ExceptionResponse handleUnexpectedMethod(NotFoundException ex, HttpServletRequest request) {
+        logger.error("Request method not allowed - URI: {}, Method: {}", request.getRequestURI(), request.getMethod(), ex);
+        return new ExceptionResponse(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ExceptionResponse> handleGeneralExceptions(Exception ex, HttpServletRequest request) {
+        logger.error("Unexpected error - URI: {}, Method: {}", request.getRequestURI(), request.getMethod(), ex);
+        if (ex instanceof HttpClientErrorException) {
+            return ResponseEntity.status(((HttpClientErrorException) ex).getStatusCode()).body(new ExceptionResponse(
+                    ex.getCause().toString(),
+                    ((HttpClientErrorException) ex).getStatusCode().value(),
+                    ex.getMessage()
             ));
-        } else if (exception instanceof HttpRequestMethodNotSupportedException) {
-            return ResponseEntity.status(((HttpRequestMethodNotSupportedException) exception).getStatusCode()).body(new ExceptionResponse(
-                    exception.getCause().getMessage(),
-                    ((HttpRequestMethodNotSupportedException) exception).getStatusCode().value(),
-                    exception.getMessage()));
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(new ExceptionResponse(exception.getMessage()));
         }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(new ExceptionResponse("An unexpected error occurred."));
     }
 }
