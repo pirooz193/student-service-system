7eb1ca6ce43fc201151fbff9eeb1260fde5d75a6
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-16 16:36:58 +0330

    Redactor code to get foods from web. - Update fetching type of foods of a week, get the list of 'td' and iterate them to get detect the empty meals. - Correct reading the properties values from config file. - Create method to get the required food from to dates in repository layer.

--- src/main/java/com/example/studentserviceapplication/service/FoodService.java
+++ src/main/java/com/example/studentserviceapplication/service/FoodService.java
@@ -6,7 +6,9 @@ import java.net.MalformedURLException;
 import java.util.List;
 
 public interface FoodService {
-    List<Food> getTodayFood();
+    List<Food> getTodayFood(Integer differentDay);
 
     void getAllFoodsFromWeb() throws InterruptedException, MalformedURLException;
+
+    List<Food> getThisWeekFoods(Integer from);
 }
--- src/main/java/com/example/studentserviceapplication/web/rest/FoodResource.java
+++ src/main/java/com/example/studentserviceapplication/web/rest/FoodResource.java
@@ -5,6 +5,7 @@ import com.example.studentserviceapplication.service.FoodService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.MalformedURLException;
@@ -21,14 +22,20 @@ public class FoodResource {
     }
 
     @GetMapping("/today")
-    public ResponseEntity<List<Food>> getTodayFood() {
-        List<Food> foods = foodService.getTodayFood();
+    public ResponseEntity<List<Food>> getRequiredDateFood(@RequestParam Integer differentDay) {
+        List<Food> foods = foodService.getTodayFood(differentDay);
         return ResponseEntity.ok().body(foods);
     }
 
+    @GetMapping("/this-week")
+    public ResponseEntity<List<Food>> getThisWeekFoods(@RequestParam Integer from) {
+        List<Food> thisWeekFoods = foodService.getThisWeekFoods(from);
+        return ResponseEntity.ok().body(thisWeekFoods);
+    }
+
     @GetMapping("/get-food-from-web")
-    public void getAllFoodsFromWeb() throws InterruptedException, MalformedURLException {
+    public ResponseEntity<?> getAllFoodsFromWeb() throws InterruptedException, MalformedURLException {
         foodService.getAllFoodsFromWeb();
-//        return ResponseEntity.ok();
+        return ResponseEntity.ok().build();
     }
 }
