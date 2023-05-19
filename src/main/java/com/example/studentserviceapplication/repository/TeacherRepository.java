703052fbf1f49ad9a1bb9c9f231f1e20cd2f0e0b
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-19 14:29:53 +0330

    Refactor the code and delete unused code.

--- src/main/java/com/example/studentserviceapplication/repository/TeacherRepository.java
+++ src/main/java/com/example/studentserviceapplication/repository/TeacherRepository.java
@@ -2,8 +2,6 @@ package com.example.studentserviceapplication.repository;
 
 import com.example.studentserviceapplication.domain.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,6 +10,4 @@ import java.util.Optional;
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByFullName(String fullName);
 
-    @Query("SELECT AVG(t.rate) FROM Teacher t WHERE t.id = :teacherId")
-    double findAverageRatingByTeacherId(@Param("teacherId") Long teacherId);
 }
